a01544d8471205aac7c3ce6af97c7a5990c858e6
jocelynxyq <jocelynyn_1234@163.com>
2017-11-24 20:10:47 +0800

    Mall7.0

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/bar', dataController.getBar);
 app.post('/barData', dataController.getBarData);
 
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
